84ac2baac1610f3803cfa9a44b67cf80bdc8dc7b
SuperCoderMan <2624187656@qq.com>
2020-06-05 17:02:45 +0800

    Create the project structure and add the implementation of hello_world function.

---
+++ basic/hello_world.cpp
@@ -0,0 +1,34 @@
+/*
+*┌───────────────────────────────────────┐
+*│ Project Name	:	CppExercises
+*│ File Name		:	hello_world.cpp
+*│ Description		:	hello_world function of c++ programming.
+*│ Create Time		:	2020年6月5日16:43:15
+*│ Version			:	Ver 0.0.0.1
+*│ Author			:	Yu Mo (Email:286273384@qq.com)
+*│ Modification 	:	Initial version.
+*└───────────────────────────────────────┘
+*
+*┌───────────────────────────────────────┐
+*│　             Copyright(C) 2020 Yu Mo. All rights reserved.  
+*└───────────────────────────────────────┘
+*/
+
+#include "hello_world.h"
+
+using namespace std;
+
+/*
+*┌───────────────────────────────────────┐
+*│ Function Name	:	hello_world                                   
+*│ Description		:	Print "Hello world".
+*│ Parameter		:	N/A
+*│ Return			:	N/A
+*│ Create Time		:	2020年6月5日16:43:22
+*│ Author			:	Yu Mo (Email:286273384@qq.com)
+*└───────────────────────────────────────┘
+*/
+void hello_world(void)
+{
+	cout << "Hello world!" << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+*┌───────────────────────────────────────┐
+*│ Project Name	:	CppExercises
+*│ File Name		:	main.cpp
+*│ Description		:	N/A
+*│ Create Time		:	2020年6月5日16:42:44
+*│ Version			:	Ver 0.0.0.1
+*│ Author			:	Yu Mo (Email:286273384@qq.com)
+*│ Modification 	:	Initial version.
+*└───────────────────────────────────────┘
+*
+*┌───────────────────────────────────────┐
+*│　             Copyright(C) 2020 Yu Mo. All rights reserved.
+*└───────────────────────────────────────┘
+*/
+
+#include <iostream>
+#include "basic/hello_world.h"
+
+using namespace std;
+
+/*
+*┌───────────────────────────────────────┐
+*│ Function Name	:	main                                   
+*│ Description		:	Main function of the project.
+*│ Parameter		:	N/A
+*│ Return			:	N/A
+*│ Create Time		:	2020年6月5日16:42:54
+*│ Author			:	Yu Mo (Email:286273384@qq.com)
+*└───────────────────────────────────────┘
+*/
+int main(int argc, char* argv[])
+{
+	hello_world();
+
+	return 0;
+}
